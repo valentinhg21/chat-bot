e97a004bd5037905ca56208f110cf6403800846a
Valentin Gutierrez <valen21hg@outlook.com>
2024-01-16 00:13:35 -0300

    Se agrego TURSO SQL y se guardo en la DB los mensajes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import logger from 'morgan'
+import dotenv from 'dotenv/config'
 import { Server } from  'socket.io'
 import { createServer } from 'node:http'
-
+import { createClient } from '@libsql/client'
 const PORT = process.env.PORT ?? 3000
 const app = express();
 const server = createServer(app);
@@ -12,7 +13,19 @@ const io = new Server(server, {
     }
 })
 
-io.on('connection', (socket) => {
+const db = createClient({
+    url: process.env.DB_URL,
+    authToken: process.env.DB_TOKEN
+})
+
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT
+  )
+`)
+io.on('connection', async (socket) => {
     console.log('a user has connected!')
 
     // Si se desconecta
@@ -20,9 +33,34 @@ io.on('connection', (socket) => {
         console.log('an user has disconnect!')
     })
     // Si se envia un mensaje
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg)
+    socket.on('chat message', async (msg) => {
+        let result
+        try {
+            result = await db.execute({
+                sql: `INSERT INTO messages (content) VALUES (:msg) `,
+                args: { msg }
+            })
+        } catch (err) {
+            console.error(err)
+            return
+        }
+        io.emit('chat message', msg, result.lastInsertRowid.toString())
     })
+
+    if(!socket.recovered){
+        try {
+            const results = await db.execute({
+                sql: 'SELECT id, content FROm messages WHERE id > ?',
+                args: [socket.handshake.auth.serverOffset ?? 0]
+            })
+            results.rows.forEach(row => {
+                socket.emit('chat message', row.content, row.id.toString())
+            });
+        } catch (error) {
+            console.error(error);
+            return
+        }
+    }
 })
 
 // Middlewares

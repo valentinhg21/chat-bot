881d21fc890caf754d9eaeaaab3840bb264bc0f4
Valentin Gutierrez <valen21hg@outlook.com>
2024-01-16 23:19:50 -0300

    Finalizando del proyecto

--- server/index.js
+++ server/index.js
@@ -22,7 +22,8 @@ const db = createClient({
 await db.execute(`
   CREATE TABLE IF NOT EXISTS messages (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    content TEXT
+    content TEXT,
+    user TEXT
   )
 `)
 io.on('connection', async (socket) => {
@@ -35,26 +36,28 @@ io.on('connection', async (socket) => {
     // Si se envia un mensaje
     socket.on('chat message', async (msg) => {
         let result
+        const username = socket.handshake.auth.username ?? 'anonymous'
         try {
+        
             result = await db.execute({
-                sql: `INSERT INTO messages (content) VALUES (:msg) `,
-                args: { msg }
+                sql: `INSERT INTO messages (content, user) VALUES (:msg, :username) `,
+                args: { msg, username}
             })
         } catch (err) {
             console.error(err)
             return
         }
-        io.emit('chat message', msg, result.lastInsertRowid.toString())
+        io.emit('chat message', msg, result.lastInsertRowid.toString(), username)
     })
 
     if(!socket.recovered){
         try {
             const results = await db.execute({
-                sql: 'SELECT id, content FROm messages WHERE id > ?',
+                sql: 'SELECT id, content, user FROM messages WHERE id > ?',
                 args: [socket.handshake.auth.serverOffset ?? 0]
             })
             results.rows.forEach(row => {
-                socket.emit('chat message', row.content, row.id.toString())
+                socket.emit('chat message', row.content, row.id.toString(), row.user)
             });
         } catch (error) {
             console.error(error);
